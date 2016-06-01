564000c18d1b56718e71a33f692b61a47a9f6a33
Pat Marshall <pmarshall1716@gmail.com>
2016-06-01 16:20:14 -0600

    pat's 2nd changes

---
+++ app/controllers/landing_page_controller.rb
@@ -0,0 +1,13 @@
+class LandingPageController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
- 
+  root 'landing_page#index'
+
+  resources :lists do 
+    resources :items
+  end
+
   devise_for :users
 end
