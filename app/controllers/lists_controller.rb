d803f5bfc7a24374c5daea5433aad362afa33f9e
Lindsey <lindsey.font@gmail.com>
2016-06-01 16:55:34 -0600

    index, new, and create controllers

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,42 @@
 class ListsController < ApplicationController
+  before_action only: [:show, :edit, :update, :destroy]
   def index
+    @lists = current_user.lists
   end
 
   def show
   end
 
+  def new
+    @list = current_user.lists.new
+  end
+
+  def create
+    @list = current_user.lists.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+  end
+
   def edit
   end
 
-  def new
+  def update
   end
+
+
+  def destroy
+  end
+
+  private
+
+    def list_params
+      params.require(:lists).permit(:name, :description)
+    end
+
+    def list
+      @list = current_user.lists.find(params[:id])
+    end
+
 end
